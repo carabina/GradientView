02c44a0d78fc4f367381572283c05cd9f5ce1eb8
jseidman95 <seidmanjesse@gmail.com>
2017-08-30 09:24:17 -0400

    Initial Commit

---
+++ GradientView/GradientView.h
@@ -0,0 +1,19 @@
+//
+//  GradientView.h
+//  GradientView
+//
+//  Created by Jesse Seidman on 8/30/17.
+//  Copyright © 2017 Jesse Seidman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GradientView.
+FOUNDATION_EXPORT double GradientViewVersionNumber;
+
+//! Project version string for GradientView.
+FOUNDATION_EXPORT const unsigned char GradientViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GradientView/PublicHeader.h>
+
+
